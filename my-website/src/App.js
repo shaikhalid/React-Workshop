7b7073d74c912f9385be2c21e892955a4e9d8d1e
Mario <mario.dsa123@gmail.com>
2019-10-02 19:12:51 +0530

    nav from react-boot

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
 import { Container } from "./components/Container/Container.jsx";
+import { NavbarTop } from "./components/NavbarTop/NavbarTop.jsx";
 
 class App extends React.Component {
   state = {};
+
   render() {
-    return <Container title="Shaikh Khalid" content="not much" />;
+    return (
+      <Container title="Shaikh Khalid" content="not much">
+        <NavbarTop></NavbarTop>
+      </Container>
+    );
   }
 }
 
